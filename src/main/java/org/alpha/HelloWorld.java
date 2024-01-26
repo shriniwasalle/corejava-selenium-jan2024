98c3f023529c4c951a82e4cc2f0262f00d16033a
shriniwasalle <shriniwasalle4@gmail.com>
2024-01-26 22:13:57 +0530

    Created a new project for git demo

---
+++ src/main/java/org/alpha/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.alpha;
+
+public class HelloWorld {
+
+    // shortcut = main
+    public static void main(String[] args) {
+        // shortcut = sout
+        System.out.println("Hello Guys, Welcome to Automation Course!");
+    }
+}
