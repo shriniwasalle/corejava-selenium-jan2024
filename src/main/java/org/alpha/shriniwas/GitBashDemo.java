e7bc61f4d2639c727576ceabcf4aed2d4483a60a
shriniwasalle <shriniwasalle4@gmail.com>
2024-01-27 23:57:51 +0530

    added update

--- src/main/java/org/alpha/shriniwas/GitBashDemo.java
+++ src/main/java/org/alpha/shriniwas/GitBashDemo.java
@@ -3,6 +3,6 @@ package org.alpha.shriniwas;
 public class GitBashDemo {
     public static void main(String[] args) {
         System.out.println("Pushing code using Git Commands");
-        System.out.println("");
+        System.out.println("Pushing code");
     }
 }
