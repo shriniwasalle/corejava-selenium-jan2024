7cc96b698af413de5dd6baa36f5e737596e5f63e
shriniwasalle <shriniwasalle4@gmail.com>
2024-01-29 21:23:32 +0530

    Added 2 files for commands demo

---
+++ src/main/java/org/alpha/shriniwas/GitBashCommands.java
@@ -0,0 +1,7 @@
+package org.alpha.shriniwas;
+
+public class GitBashCommands {
+    public static void main(String[] args) {
+        System.out.println("Pushing the changes through Git Bash");
+    }
+}
