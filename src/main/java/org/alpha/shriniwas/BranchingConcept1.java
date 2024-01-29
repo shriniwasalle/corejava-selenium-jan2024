da2927b810f496538ba5405628423a65a182d345
shriniwasalle <shriniwasalle4@gmail.com>
2024-01-29 22:07:29 +0530

    ALPHA-SHRINIWAS-1: Addressed the review comments

--- src/main/java/org/alpha/shriniwas/BranchingConcept1.java
+++ src/main/java/org/alpha/shriniwas/BranchingConcept1.java
@@ -2,6 +2,6 @@ package org.alpha.shriniwas;
 
 public class BranchingConcept1 {
     public static void main(String[] args) {
-        System.out.println("Pushing the code to new branch");
+        System.out.println("Adding message after review comment");
     }
 }
