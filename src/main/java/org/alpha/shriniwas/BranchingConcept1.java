05e800552f129018b1f89f896096e4f656357b92
shriniwasalle <shriniwasalle4@gmail.com>
2024-01-29 21:55:41 +0530

    ALPHA-SHRINIWAS-1: Updated a file

--- src/main/java/org/alpha/shriniwas/BranchingConcept1.java
+++ src/main/java/org/alpha/shriniwas/BranchingConcept1.java
@@ -1,4 +1,7 @@
 package org.alpha.shriniwas;
 
 public class BranchingConcept1 {
+    public static void main(String[] args) {
+        System.out.println("Pushing the code to new branch");
+    }
 }
