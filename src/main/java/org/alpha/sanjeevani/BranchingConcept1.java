c19ce0358df531f1a1cb50ff55f788f4716f59f5
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-01-30 17:20:24 +0530

    ALPHA-SANJEEVANI-1: Learning git branching concept

---
+++ src/main/java/org/alpha/sanjeevani/BranchingConcept1.java
@@ -0,0 +1,9 @@
+package org.alpha.sanjeevani;
+
+
+
+public class BranchingConcept1 {
+    public static void main(String[] args) {
+        System.out.println("To learning new branching concept");
+    }
+}
