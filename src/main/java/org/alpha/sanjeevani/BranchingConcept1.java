8dd28afe71c3d5c7d39421051c0b1879dccd8308
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-02 00:01:52 +0530

    ALPHA-SANJEEVANI-2: Added a new file of conflict

--- src/main/java/org/alpha/sanjeevani/BranchingConcept1.java
+++ src/main/java/org/alpha/sanjeevani/BranchingConcept1.java
@@ -1,6 +1,10 @@
 package org.alpha.sanjeevani;
+
 public class BranchingConcept1 {
     public static void main(String[] args) {
-        System.out.println("To learning new branching concept");
+        System.out.println("Adding message after review comment");
+        System.out.println("Adding changes for conflicts 567");
+        String name="sanjeevani";
+        System.out.println("Name:"+name);
     }
 }
