8a1857cfa8684b94cb0383226b11d4b152be7f0f
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-01 23:53:57 +0530

    ALPHA-SANJEEVANI-2: Added a new file of gitbashdemo

---
+++ src/main/java/org/alpha/sanjeevani/GitBashDemo.java
@@ -0,0 +1,8 @@
+package org.alpha.sanjeevani;
+
+public class GitBashDemo {
+    public static void main(String[] args) {
+        System.out.println("Pushing code using Git Commands");
+        System.out.println("Pushing code through IntelliJ");
+    }
+}
