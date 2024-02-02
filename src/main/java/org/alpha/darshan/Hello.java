a66981ad3463fcac5c3164a5a06909ad567c245a
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-02 12:24:33 +0530

    ALPHA-DARSHAN-26 Added a workflow for pull request template new practice

--- src/main/java/org/alpha/darshan/Hello.java
+++ src/main/java/org/alpha/darshan/Hello.java
@@ -2,6 +2,6 @@ package org.alpha.darshan;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("new practice example");
+        System.out.println("new practice example new ");
     }
 }
