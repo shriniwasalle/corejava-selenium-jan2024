af09dd880b3da127bf659f58da6850766b2e3f3a
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-02 12:12:07 +0530

    ALPHA-DARSHAN-26 Added a workflow for pull request template new

--- src/main/java/org/alpha/darshan/GitPractice.java
+++ src/main/java/org/alpha/darshan/GitPractice.java
@@ -4,6 +4,6 @@ public class GitPractice {
     public static void main(String[] args) {
 
         System.out.println("practice");
-        System.out.println("git");
+        System.out.println("gitbash");
     }
 }
