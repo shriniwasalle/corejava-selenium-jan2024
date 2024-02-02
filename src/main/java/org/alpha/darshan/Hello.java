8de962a73b8863fbeb051a52a3c81276bd72e019
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-02 12:53:45 +0530

    ALPHA-DARSHAN-26 new merge conflict example new from master new one example

--- src/main/java/org/alpha/darshan/Hello.java
+++ src/main/java/org/alpha/darshan/Hello.java
@@ -2,6 +2,6 @@ package org.alpha.darshan;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("merge conflict example from master new one ");
+        System.out.println("merge conflict example from master new one example ");
     }
 }
