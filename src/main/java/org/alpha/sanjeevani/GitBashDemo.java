3a70c584f66a187a76280df6e26b1405559c11ac
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-03 17:52:59 +0530

    ALPHA-SANJEEVANI-2: Added a new content into exiting file

--- src/main/java/org/alpha/sanjeevani/GitBashDemo.java
+++ src/main/java/org/alpha/sanjeevani/GitBashDemo.java
@@ -3,6 +3,8 @@ package org.alpha.sanjeevani;
 public class GitBashDemo {
     public static void main(String[] args) {
         System.out.println("Pushing code using Git Commands");
+        String name="Sanjeevani123";
+        System.out.println("name"+name);
         System.out.println("Pushing code through IntelliJ");
     }
 }
