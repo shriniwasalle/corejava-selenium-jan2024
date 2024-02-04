ff90764f3aeaeb16685983d88ea707a0bef53bc4
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-04 08:37:49 +0530

    ALPHA-SANJEEVANI-3: adding for conflits

--- src/main/java/org/alpha/shriniwas/BranchingConcept1.java
+++ src/main/java/org/alpha/shriniwas/BranchingConcept1.java
@@ -3,7 +3,7 @@ package org.alpha.shriniwas;
 public class BranchingConcept1 {
     public static void main(String[] args) {
         System.out.println("Adding message after review comment");
-        String name = "Shriniwas";
+        String name = "Shriniwas1234";
         System.out.println("Name:" +name);
     }
 }
