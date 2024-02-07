c08db03f244336799f6bcedea65972e0696bec28
shriniwasalle <shriniwasalle4@gmail.com>
2024-02-07 22:41:31 +0530

    Variables

---
+++ src/main/java/org/alpha/shriniwas/variables/LocalVariables.java
@@ -0,0 +1,32 @@
+package org.alpha.shriniwas.variables;
+
+public class LocalVariables {
+
+    public static void main(String[] args) {
+        int age = 10;
+        // age is a variable of type int
+
+        // int : primitive date type
+        // age : variable
+        // 10 : value
+        // = : assignment operator
+
+        boolean bb = true;
+        // bb is a variable of type boolean
+
+        char ch = 'S'; // need to use single quotes
+        String name = "Shriniwas"; // need to use double quotes
+        float f = 100.5f;
+        double d = 100.5;
+
+        int [] st = { 100 , 200, 300};
+
+
+        System.out.println(age);
+        System.out.println(bb);
+        System.out.println(ch);
+        System.out.println(name);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
