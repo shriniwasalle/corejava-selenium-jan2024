36347d4de35bee79ea7bcdf5c06f05d1947ae9ad
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-08 10:37:37 +0530

    ALPHA-SANJEEVANI-01: created a new program of default variables

---
+++ src/main/java/org/alpha/sanjeevani/variables/TestVariables1.java
@@ -0,0 +1,22 @@
+package org.alpha.sanjeevani.variables;
+
+public class TestVariables1 {
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean bb;
+    static char ch;
+    public static void main(String[] args) {
+        System.out.println("byte : "+ b);
+        System.out.println("short : "+ s);
+        System.out.println("int : "+ i);
+        System.out.println("long : "+ l);
+        System.out.println("float : "+ f);
+        System.out.println("double : "+ d);
+        System.out.println("boolean : "+ bb);
+        System.out.println("char : "+ ch);
+    }
+}
