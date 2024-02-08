1e13abf0d12ed74f4ebe16d40756bc2be1247487
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-08 11:42:48 +0530

    ALPHA-SANJEEVANI-01: created a new program of variables2

---
+++ src/main/java/org/alpha/sanjeevani/variables/TestVariables2.java
@@ -0,0 +1,23 @@
+package org.alpha.sanjeevani.variables;
+
+public class TestVariables2 {
+    public static void main(String[] args) {
+         byte b=-127;
+         short s=-500;
+         int i=254;
+         long l=567;
+         float f=56.67f;
+         double d=345.678;
+         boolean bb=true;
+         char ch='S';
+        System.out.println(" [value of byte] :" +b);
+        System.out.println("[value of short] : "+ s);
+        System.out.println("[value of int] : " +i);
+        System.out.println("[value of long] : "+l);
+        System.out.println("[value of float] : "+ f);
+        System.out.println("[value of double] : " +d);
+        System.out.println("[value of boolean] : "+bb);
+        System.out.println("[value of char] : "+ch);
+
+    }
+}
