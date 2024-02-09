2d3712bf1437ffe0c0a10989ad12548fb9ebd7a5
shrutimushan <shrutimushan168@gmail.com>
2024-02-09 22:01:21 +0530

    ALPHA-SHRUTI-8: Learning Git Branch Concept

--- src/main/java/org/alpha/shruti/BranchConcept.java
+++
@@ -1,7 +0,0 @@
-package org.alpha.shruti;
-
-public class BranchConcept {
-    public static void main(String[] args) {
-        System.out.println("Adding message after review commannd");
-    }
-}
