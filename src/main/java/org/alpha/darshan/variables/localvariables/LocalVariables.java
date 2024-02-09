381ed0dee5f2d1855bf5151ce61931613afdba02
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-09 20:40:00 +0530

    ALPHA-DARSHAN-26 Variable practice

---
+++ src/main/java/org/alpha/darshan/variables/localvariables/LocalVariables.java
@@ -0,0 +1,18 @@
+package org.alpha.darshan.variables.localvariables;
+
+public class LocalVariables {
+    public static void main(String[] args) {
+         byte b=125;
+
+         int in=10221;
+
+         float f= 10.2f;
+
+         char ch='d';
+
+        System.out.println("b");
+        System.out.println("in");
+        
+
+    }
+}
