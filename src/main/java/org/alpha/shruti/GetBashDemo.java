fd4502f4f237f71d17bed8d1e1c588f24dc51682
shrutimushan <shrutimushan168@gmail.com>
2024-02-09 20:12:31 +0530

    Added one more file for Git Commands

---
+++ src/main/java/org/alpha/shruti/GetBashDemo.java
@@ -0,0 +1,10 @@
+package org.alpha.shruti;
+
+public class GetBashDemo {
+    public static void main(String[] args) {
+        System.out.println("Pushing code using Git Commands");
+        String name = "shrutimushan";
+        System.out.println("name" + name);
+        System.out.println("Pushing code through Intellji");
+    }
+}
