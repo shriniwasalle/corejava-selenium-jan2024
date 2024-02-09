39890faab03e2ddd9c70185f8be532ecf802f630
shrutimushan <shrutimushan168@gmail.com>
2024-02-09 16:07:03 +0530

    created a new project for git demo

---
+++ src/main/java/org/alpha/shruti/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.alpha.shruti;
+
+public class HelloWorld {
+    //shortcut=main
+    public static void main(String[] args) {
+     //shortcut=sout
+        System.out.println("Hello guys, welcome to Automation");
+        
+    }
+}
