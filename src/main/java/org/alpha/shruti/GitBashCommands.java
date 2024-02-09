394f1a603f504616491b5fa7734a281e7871ca91
shrutimushan <shrutimushan168@gmail.com>
2024-02-09 20:46:30 +0530

    "Added one more file for Git Commands"

--- src/main/java/org/alpha/shruti/GitBashCommands.java
+++ src/main/java/org/alpha/shruti/GitBashCommands.java
@@ -1,2 +1,8 @@
-package org.alpha.shruti;public class GitBashCommands {
+package org.alpha.shruti;
+
+public class GitBashCommands {
+    public static void main(String[] args) {
+        System.out.println("Pushing the code through Commands");
+
+    }
 }
