922c87654c6e340ee623bfadbdd751b60e92ddc6
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-10 19:21:53 +0530

    ALPHA-SANJEEVANI-01: created program files of static variable.

---
+++ src/main/java/org/alpha/sanjeevani/variables/TestStaticVariables1.java
@@ -0,0 +1,24 @@
+package org.alpha.sanjeevani.variables;
+
+public class TestStaticVariables1 {
+    static int age=24;
+    static String name="sanjana";
+
+    //non-static method/instance method we can access directly
+    public void m1(){
+        System.out.println(age);
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        //using classname
+        System.out.println(TestStaticVariables1.age);
+        System.out.println(TestStaticVariables1.name);
+        System.out.println("----------------------");
+          //Direct Access
+        System.out.println(age);
+        System.out.println(name);
+
+
+
+    }
+}
