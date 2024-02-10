b842c1fc9f2e7871cb85bf9ec14dafaab19bb80f
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-10 19:39:07 +0530

    ALPHA-SANJEEVANI-01: created program files of methods.

---
+++ src/main/java/org/alpha/sanjeevani/methods/TestMethodWithoutParam.java
@@ -0,0 +1,17 @@
+package org.alpha.sanjeevani.methods;
+
+public class TestMethodWithoutParam {
+    public void m1() {
+        System.out.println("I am in m1 method");
+    }
+        public void m2(){
+            System.out.println("I am in m2 method");
+        }
+
+    public static void main(String[] args) {
+        TestMethodWithoutParam obj=new TestMethodWithoutParam();
+        obj.m1();
+        obj.m2();
+
+    }
+}
