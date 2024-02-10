f23945aff5e2566799b995723939305643b24fb8
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-10 10:36:21 +0530

    ALPHA-SANJEEVANI-01: created program files of instance variable.

---
+++ src/main/java/org/alpha/sanjeevani/variables/InstanceVariable.java
@@ -0,0 +1,20 @@
+package org.alpha.sanjeevani.variables;
+
+public class InstanceVariable {
+    //<----------------instance variable---------->
+    int age=100;
+    String name="sanjeevani";
+    boolean flag=false;
+    //<----------------instance method---------->
+    public void m1(){
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(flag);
+    }
+    //<----------------static method---------->
+    public static void main(String[] args) {
+        //<----------------local variable---------->
+        InstanceVariable obj=new InstanceVariable();
+        obj.m1();
+    }
+}
