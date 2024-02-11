5e3a922ea041c504cf7ec3af4ae9d00672414fde
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-11 08:33:57 +0530

    ALPHA-SANJEEVANI-01: created program files of all 3 variables.

---
+++ src/main/java/org/alpha/sanjeevani/variables/TestVariables3.java
@@ -0,0 +1,16 @@
+package org.alpha.sanjeevani.variables;
+
+public class TestVariables3 {
+    int a=20;
+    static int b=30;
+
+    public static void main(String[] args) {
+        TestVariables3 ref=new TestVariables3();
+        int c=40;
+        System.out.println(c); //local variable
+        System.out.println(b); //static variable (direct access)
+        System.out.println(TestVariables3.b);//with (class name.variable name) also
+        System.out.println(ref.a);
+    }
+
+}
