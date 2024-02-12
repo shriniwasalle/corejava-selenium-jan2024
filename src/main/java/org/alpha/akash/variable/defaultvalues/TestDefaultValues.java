5c6bba22ba912b9ef739ec981a35b311643c8eb5
Akash Vaitala <119657113+akashvaitala05@users.noreply.github.com>
2024-02-12 20:55:01 +0530

    Core Java Concept of Static Variable

---
+++ src/main/java/org/alpha/akash/variable/defaultvalues/TestDefaultValues.java
@@ -0,0 +1,59 @@
+package org.alpha.akash.variable.defaultvalues;
+
+import org.alpha.akash.variable.Employee;
+import org.alpha.akash.variable.Student;
+public class TestDefaultValues {
+    static byte b;
+    static  short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static boolean bb;
+    static  char ch;
+    static  String str;
+    static StringBuffer sb;
+    static Student st;
+    static Employee emp;
+    static String [] names;
+
+    public static void main(String[] args) {
+        //primitive Data Types
+        System.out.println("byte----->"+b);
+
+        System.out.println("short----->"+s);
+
+        System.out.println("int----->"+i);
+
+        System.out.println("long----->"+l);
+
+        System.out.println("float----->"+f);
+
+        System.out.println("double----->"+d);
+
+        System.out.println("char----->"+ch);
+
+        System.out.println("boolean----->"+bb);
+
+        //Non Primitive Data Type
+        System.out.println("String----->"+str);
+
+        System.out.println("StringBuffer----->"+sb);
+
+        //User Defined Class
+        System.out.println("Student----->"+st);
+
+        System.out.println("Employee----->"+emp);
+
+        System.out.println("String[]----->"+names);
+
+
+
+
+
+
+    }
+
+
+
+}
