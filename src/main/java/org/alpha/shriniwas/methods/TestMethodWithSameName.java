2c7753f8603b6b8654a7594305e254c37f019aea
shriniwasalle <shriniwasalle4@gmail.com>
2024-02-13 23:05:35 +0530

    Methods

---
+++ src/main/java/org/alpha/shriniwas/methods/TestMethodWithSameName.java
@@ -0,0 +1,16 @@
+package org.alpha.shriniwas.methods;
+
+public class TestMethodWithSameName {
+
+//    public void empData(int a, int b) {
+//      System.out.println("Data : " +a+b);
+//    }
+//
+//    public void empData(int c, int d) {
+//        System.out.println("Data : " +c+d);
+//    }
+
+    public static void main(String[] args) {
+        TestMethodWithSameName obj = new TestMethodWithSameName();
+    }
+}
