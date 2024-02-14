71685bd68ed807f9f23bc0bd194f2237c85e3eaa
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-14 18:26:54 +0530

    ALPHA-SANJEEVANI-01: merge comflit resolve.

--- src/main/java/org/alpha/sanjeevani/methods/Employee.java
+++ src/main/java/org/alpha/sanjeevani/methods/Employee.java
@@ -2,6 +2,11 @@ package org.alpha.sanjeevani.methods;
 
 public class Employee {
     String empName="sanjeevani";
-    int empId=101;
-    float empSalary=555.55f;
+    int empId=100;
+    float salary=700.70f;
+    public void getEmpData(){
+        System.out.println(empName);
+        System.out.println(empId);
+        System.out.println(salary);
+    }
 }
