f5a79f0cb2c138fd1eef2de71409f3621d273d10
tejashriboppa <tejashriboppa@gmail.com>
2024-02-17 19:59:48 +0530

    Core java Practice

--- src/main/java/org/alpha/tejashri/TestHelloWorld.java
+++ src/main/java/org/alpha/tejashri/TestHelloWorld.java
@@ -3,6 +3,8 @@ package org.alpha.tejashri;
 public class TestHelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Automation");
+
+        System.out.println("Hello World");
     }
 
 }
