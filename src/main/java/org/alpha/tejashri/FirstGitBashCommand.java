ed17d72b72c90164f486786b92cde554cf06608a
tejashriboppa <tejashriboppa@gmail.com>
2024-02-18 14:14:57 +0530

    Added 2 files for commands demo

---
+++ src/main/java/org/alpha/tejashri/FirstGitBashCommand.java
@@ -0,0 +1,7 @@
+package org.alpha.tejashri;
+
+public class FirstGitBashCommand {
+    public static void main(String[] args) {
+        System.out.println("First File for Testing");
+    }
+}
---
+++ src/main/java/org/alpha/tejashri/SecondGitBashCommand.java
@@ -0,0 +1,7 @@
+package org.alpha.tejashri;
+
+public class SecondGitBashCommand {
+    public static void main(String[] args) {
+        System.out.println("Second File for Testing");
+    }
+}
