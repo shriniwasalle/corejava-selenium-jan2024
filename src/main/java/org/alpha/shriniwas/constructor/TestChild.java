e9d1d2fa228a59624996b88314d3be0223f52d72
shriniwasalle <shriniwasalle4@gmail.com>
2024-02-19 22:54:42 +0530

    Pushing to master

--- src/main/java/org/alpha/shriniwas/constructor/TestChild.java
+++ src/main/java/org/alpha/shriniwas/constructor/TestChild.java
@@ -2,12 +2,36 @@ package org.alpha.shriniwas.constructor;
 
 public class TestChild extends TestParent {
 
+    String empName;
+
     public TestChild() {
-        super("Shriniwas");
+        super();
         System.out.println("Inside TestChild");
     }
 
+    public TestChild(int age) {
+        super("Shri");
+        System.out.println(age);
+    }
+
+    public TestChild(String name, int age, String empName) {
+        this(1000);
+        this.empName = empName;
+        System.out.println(super.empName);
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+    public void TestChild(float f) {
+        System.out.println(f);
+    }
+
     public static void main(String[] args) {
-        TestChild obj = new TestChild();
+        // TestChild obj = new TestChild();
+        // TestChild obj1 = new TestChild(10);
+
+        TestChild obj2 = new TestChild("Shrini", 100, "XYZ");
+        obj2.TestChild(10.5f);
+        // TestParent obj = new TestParent('B'); We can not create an Object for a private constructor
     }
 }
