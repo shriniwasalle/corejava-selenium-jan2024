178465f2e1106fcfa0aa1582590996dd099fffcc
tejashriboppa <tejashriboppa@gmail.com>
2024-02-19 18:25:14 +0530

    commit

---
+++ src/main/java/org/alpha/tejashri/variables/TestDefaultValues.java
@@ -0,0 +1,33 @@
+package org.alpha.tejashri.variables;
+
+public class TestDefaultValues {
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static char ch;
+    static boolean bb;
+    static String str;
+
+
+
+    public static void main(String[] args) {
+        System.out.println("**********Primitive Data Types********");
+        System.out.println("byte---->" +b); // by default value is 0
+        System.out.println("short---->" +s);
+        System.out.println("int--->" +i);
+        System.out.println("long---->" +l);
+        System.out.println("float--->" +f);
+        System.out.println("double---->" +d);
+        System.out.println("character---->" +ch);
+        System.out.println("boolean---->" +bb);
+
+        System.out.println("********** Non Primitive Data Types********");
+
+        System.out.println("String---->" +str);
+
+        System.out.println("********** Non Primitive Data Types********");
+    }
+}
