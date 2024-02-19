9763dab0a7b00bd4cdd05f45a03a9fd81bb1c7d9
tejashriboppa <tejashriboppa@gmail.com>
2024-02-19 13:47:49 +0530

    ALPHA-TEJASHRI-04-2: git conflicts

--- src/main/java/org/alpha/tejashri/GitConflicts.java
+++ src/main/java/org/alpha/tejashri/GitConflicts.java
@@ -2,6 +2,7 @@ package org.alpha.tejashri;
 
 public class GitConflicts {
     public static void main(String[] args) {
+        System.out.println("Adding massage after review comment");
         System.out.println("Conflicts 123");
     }
 }
