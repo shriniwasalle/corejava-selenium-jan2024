fed12590f360609b8e8ed526a4002b968754b5eb
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-19 14:18:31 +0530

    ALPHA-SANJEEVANI-01: created constructor program files .

---
+++ src/main/java/org/alpha/sanjeevani/constructor/TestConstructor1.java
@@ -0,0 +1,17 @@
+package org.alpha.sanjeevani.constructor;
+
+public class TestConstructor1 {
+    TestConstructor1(){
+        //0-args constructor/no-args constructor/0-param constructor
+        System.out.println("i am in no-args constructor");
+    }
+
+    public static void main(String[] args) {
+        TestConstructor1 obj= new TestConstructor1();
+    }
+}
+//TestConstructor1 =class
+//obj=reference variable
+//=assignment operator
+//new =keyword (It is used to crete an object)
+//TestConstructor1()=constructor
