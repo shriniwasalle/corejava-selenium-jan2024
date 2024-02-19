5fd191ad5a3312fd1ff88fbce277aa16e6ebd815
tejashriboppa <tejashriboppa@gmail.com>
2024-02-19 23:34:04 +0530

    pushing the changes to master

--- src/main/java/org/alpha/tejashri/TestHelloShri.java
+++ src/main/java/org/alpha/tejashri/TestHelloShri.java
@@ -1,4 +1,7 @@
 package org.alpha.tejashri;
 
 public class TestHelloShri {
+    public static void main(String[] args) {
+        System.out.println("456");
+    }
 }
--- src/main/java/org/alpha/tejashri/TestNewBranch.java
+++ src/main/java/org/alpha/tejashri/TestNewBranch.java
@@ -2,6 +2,6 @@ package org.alpha.tejashri;
 
 public class TestNewBranch {
     public static void main(String[] args) {
-
+        System.out.println("adding a new line");
     }
 }
