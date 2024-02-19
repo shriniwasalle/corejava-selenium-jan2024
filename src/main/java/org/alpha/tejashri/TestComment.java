7fe87ea6e4aaae69c8d7cabbdc4bafe686914cd3
tejashriboppa <tejashriboppa@gmail.com>
2024-02-19 15:48:22 +0530

    ALPHA-TEJASHRI-04-2: Test comments

---
+++ src/main/java/org/alpha/tejashri/TestComment.java
@@ -0,0 +1,26 @@
+package org.alpha.tejashri;
+
+/** This is documentation comment
+ * @author : Tejashri
+ * @since : 2024
+ *
+ *
+ *
+ */
+public class TestComment {
+
+    public static void main(String[] args) {
+        //System.out.println("First Line"); Single line comment
+        System.out.println("Second Line");
+        System.out.println("Third Line");
+        /*System.out.println("First Line"); Multiline comment
+        System.out.println("Second Line");
+        System.out.println("Third Line");
+        System.out.println("First Line");*/
+        System.out.println("Second Line");
+        System.out.println("Third Line");
+        System.out.println("First Line");
+        System.out.println("Second Line");
+        System.out.println("Third Line");
+    }
+}
