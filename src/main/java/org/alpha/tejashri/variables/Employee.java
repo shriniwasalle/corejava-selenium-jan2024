fa341e09d0a31f7b9b66237f92e882f59f8d3c34
tejashriboppa <tejashriboppa@gmail.com>
2024-02-20 19:37:41 +0530

    Employee

--- src/main/java/org/alpha/tejashri/variables/Employee.java
+++ src/main/java/org/alpha/tejashri/variables/Employee.java
@@ -2,5 +2,5 @@ package org.alpha.tejashri.variables;
 
 public class Employee {
     // Instance Variable
-    public String companyName = "Google";
+    public String companyName = "Microsoft";
     }
