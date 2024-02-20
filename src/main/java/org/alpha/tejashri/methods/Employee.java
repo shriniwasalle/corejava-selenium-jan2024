15c91fd1f6cbfa1cd28f35143f7df94b9e521c03
tejashriboppa <tejashriboppa@gmail.com>
2024-02-20 18:19:07 +0530

    employee

---
+++ src/main/java/org/alpha/tejashri/methods/Employee.java
@@ -0,0 +1,9 @@
+package org.alpha.tejashri.methods;
+
+public class Employee {
+    public void getEmpData (){
+        System.out.println("Tejashri");
+        System.out.println("Boppa");
+        System.out.println("Google");
+    }
+}
