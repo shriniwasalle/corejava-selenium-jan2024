4a5aeadc609f3e0e280309da50b6c58bd5d7d071
tejashriboppa <tejashriboppa@gmail.com>
2024-02-20 18:48:31 +0530

    with Parameter

---
+++ src/main/java/org/alpha/tejashri/methods/TestMethodWithParameter.java
@@ -0,0 +1,22 @@
+package org.alpha.tejashri.methods;
+
+public class TestMethodWithParameter {
+    public void addition (int a, int b)
+    {
+        System.out.println(a+b); // 100+200 , 200+400
+    }
+
+    public void addition (int a, int b, int c)
+    {
+        System.out.println(a+b+c); // 100+200+300 , 200+400+500
+    }
+
+    public static void main(String[] args) {
+        TestMethodWithParameter obj = new TestMethodWithParameter();
+        obj.addition(100, 200);
+        obj.addition(10, 20); // called with same method with different data
+        obj.addition(200, 400); // called with same method with different data
+        obj.addition(100, 200, 300);
+        obj.addition(10, 20,30); // called with same method with different data
+    }
+}
