74cb079ba3575cb17d3d5596ebd78aa2cfa63c99
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-21 20:43:51 +0530

    Core Java Concept

---
+++ src/main/java/org/alpha/darshan/custructors/TestChild.java
@@ -0,0 +1,18 @@
+package org.alpha.darshan.custructors;
+
+public class TestChild extends TestParent {
+
+    public TestChild(){
+        System.out.println("im in child method");
+    }
+
+    public TestChild(String name){
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        TestChild t1=new TestChild();
+
+        TestChild t2= new TestChild("DARSHAN");
+    }
+}
---
+++ src/main/java/org/alpha/darshan/custructors/TestParent.java
@@ -0,0 +1,11 @@
+package org.alpha.darshan.custructors;
+
+public class TestParent {
+
+    public TestParent(){
+        System.out.println("im in test parent");
+    }
+    public static void main(String[] args) {
+        TestParent t1= new TestParent();
+    }
+}
