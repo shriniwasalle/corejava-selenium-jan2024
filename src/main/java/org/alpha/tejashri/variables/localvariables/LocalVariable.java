2aa8a13b384b1d28f9b901dfc265d791140060e6
tejashriboppa <tejashriboppa@gmail.com>
2024-02-22 20:54:14 +0530

    ALPHA-TEJASHRI-1: Local Variables

--- src/main/java/org/alpha/tejashri/variables/localvariables/LocalVariable.java
+++ src/main/java/org/alpha/tejashri/variables/localvariables/LocalVariable.java
@@ -1,17 +1,19 @@
 package org.alpha.tejashri.variables.localvariables;
 
-//import org.alpha.tejashri.variables.Employee;
+import org.alpha.tejashri.variables.Employee;
+
 public class LocalVariable {
-    public static <Employee> void main(String[] args) {
+    public static void main(String[] args) {
         int age = 10; // how to read ---> age is a variable of  type int
         /* int : primitive data type
         age : variable
         10 : value
         = : assignment operator */
+
         boolean bb = true;
         //bb is a variable of type boolean
 
-char ch = 'S'; // need to use single quotes
+        char ch = 'S'; // need to use single quotes
         String name = "Tejashri"; // need to use double quotes
         float f = 100.5f;
         double d = 100.5;
@@ -20,13 +22,13 @@ char ch = 'S'; // need to use single quotes
         String [] names = {"Tejashri", "Mahika", "Anshika"};
         boolean [] flags = {false, false, true};
 
-//            Employee emp = new Employee();
+        Employee emp = new Employee();
 
-        /* emp = ref variable
-         Employee = class
-         new Employee() = Object */
+//        /* emp = ref variable
+//         Employee = class
+//         new Employee() = Object */
 
-//        Employee[] emps = null;
+        Employee [] emps = null;
 
         System.out.println(age);
         System.out.println(bb);
@@ -34,7 +36,6 @@ char ch = 'S'; // need to use single quotes
         System.out.println(name);
         System.out.println(f);
         System.out.println(d);
-
 
         System.out.println("*****  Array Variables ***** ");
         System.out.println(st);
@@ -45,9 +46,9 @@ char ch = 'S'; // need to use single quotes
         System.out.println(flags);
 
         System.out.println("*****  User Defined Class Variable **** ");
-//        System.out.println(emp);
+        System.out.println(emp);
 
         System.out.println("****  User Defined Class Array Variable **** ");
-//        System.out.println(emps);
+        System.out.println(emps);
    }
 }
