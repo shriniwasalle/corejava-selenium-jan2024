977d641f174c00b9d09b73656d30edbc275a0285
shriniwasalle <shriniwasalle4@gmail.com>
2024-02-23 23:07:13 +0530

    Strings

---
+++ src/main/java/org/alpha/shriniwas/stringconcept/TestString3.java
@@ -0,0 +1,11 @@
+package org.alpha.shriniwas.stringconcept;
+
+public class TestString3 {
+    public static void main(String[] args) {
+        String str1 = "Shriniwas";
+
+        str1 = str1.concat("Alle");
+
+        System.out.println(str1);
+    }
+}
