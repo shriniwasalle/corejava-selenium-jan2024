e66427b45719ae09c51acd1f4185cf4fa0db042a
aishwarya <aishwaryarathod914@gmail.com>
2024-02-23 20:01:20 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts

---
+++ src/main/java/org/alpha/aishwarya/sout/TestSout.java
@@ -0,0 +1,28 @@
+package org.alpha.aishwarya.sout;
+
+public class TestSout {
+     public static String name = "Aishwarya";
+
+     public static void main (String[] args){
+
+        // System.out.println(TestSout.name);
+       //  System.out.println(TestSout.name.length());
+         // string method =Length()
+
+         int len = TestSout.name.length();
+         System.out.println(name);
+         System.out.println(len);
+     }
+
+    /** Where
+     * TestOut : is a class
+     * .name : it is static variable of type sting
+     * .length() : method of String class
+      */
+
+    /** Where Explanation of System.out.println();
+     *  system: it is class
+     *  .out : it is static variable of type printStream class
+     *  .println(); : method of printStream class
+     */
+}
