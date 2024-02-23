d58c6f5463f000b1b4820ebcaf1ea72523e74ca7
tejashriboppa <tejashriboppa@gmail.com>
2024-02-23 16:44:24 +0530

    ALPHA-TEJASHRI-1: Instance blocks

---
+++ src/main/java/org/alpha/tejashri/blocks/instanceblocks/TestInstanceBlocks.java
@@ -0,0 +1,31 @@
+package org.alpha.tejashri.blocks.instanceblocks;
+
+public class TestInstanceBlocks {
+// Instance Block
+    {
+        System.out.println("I am in Instance Block : 2");
+    }
+    {
+        System.out.println("I am in Instance Block : 1");
+    }
+
+public TestInstanceBlocks(){
+int a = 100;
+int b= 200;
+
+System.out.println("Addition: " +(a+b));
+
+}
+    {
+        System.out.println("I am in Instance Block : 3");
+    }
+public TestInstanceBlocks (int a, int b){
+    System.out.println("Multiplication: " +a*b);
+}
+    public static void main(String[] args) {
+        TestInstanceBlocks obj1 = new TestInstanceBlocks(); // Addition
+        System.out.println("----------------------");
+        TestInstanceBlocks obj2 = new TestInstanceBlocks(10,20); // Multiplication
+    }
+
+}
