29272d8076b40c3bc371f7440a4cb970d561a038
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-02-23 07:22:28 +0530

    ALPHA-SANJEEVANI-01: created a new program of String.

---
+++ src/main/java/org/alpha/sanjeevani/stringconcept/TestString3.java
@@ -0,0 +1,11 @@
+package org.alpha.sanjeevani.stringconcept;
+
+public class TestString3 {
+    public static void main(String[] args) {
+        String str="Sanjeevani";
+
+       str=str.concat("Sutar");
+
+        System.out.println(str);
+    }
+}
