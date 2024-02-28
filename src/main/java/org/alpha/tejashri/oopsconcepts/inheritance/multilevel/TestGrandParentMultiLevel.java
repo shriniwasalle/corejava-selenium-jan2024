82f515fbddee267a67d82bcd3acbf32fb74ccd23
tejashriboppa <tejashriboppa@gmail.com>
2024-02-28 17:18:05 +0530

    ALPHA-TEJASHRI-1: Testing Multi Level Inheritance

---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/inheritance/multilevel/TestGrandParentMultiLevel.java
@@ -0,0 +1,13 @@
+package org.alpha.tejashri.oopsconcepts.inheritance.multilevel;
+
+public class TestGrandParentMultiLevel {
+    // by default this class extends the object class, so we can access all methods/variables from object class
+    int age = 100;
+    String empName = "Teja";
+    String str1 = "teja";
+    float salary = 10000.5f;
+
+    public void getEmployee() {
+        System.out.println("empName & str1 are same : " + empName.equals(str1)); // equals() method called from object class
+    }
+}
