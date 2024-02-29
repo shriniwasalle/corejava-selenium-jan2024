8fe12f3ae3b3f9d50c251ebf719c2c4b574ffade
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-02-29 21:02:27 +0530

    ALPHA-DARSHAN-26 Core Java Concept

---
+++ src/main/java/org/alpha/darshan/opponcept/inheritance/TestParentClass.java
@@ -0,0 +1,14 @@
+package org.alpha.darshan.opponcept.inheritance;
+
+public class TestParentClass {
+
+    int age=50;
+
+    public void getData(){
+        System.out.println("i am in parent class");
+    }
+    public void m1(){
+        System.out.println(" i am in test parent class");
+    }
+
+}
