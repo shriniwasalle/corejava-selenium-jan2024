00132bc3c371ea7c530fe9b049e7252bc438bcb7
shrutimushan <shrutimushan168@gmail.com>
2024-02-29 11:53:43 +0530

    comments

---
+++ src/main/java/org/alpha/shruti/TestComments.java
@@ -0,0 +1,10 @@
+package org.alpha.shruti;
+
+public class TestComments {
+    public static void main(String[] args) {
+        System.out.println("First line");
+        System.out.println("Second line");
+        System.out.println("Third line");
+
+    }
+}
