a82281aa684501bb33a459b5d0db925d5b38fabe
shrutimushan <shrutimushan168@gmail.com>
2024-02-29 12:04:19 +0530

    comments

--- src/main/java/org/alpha/shruti/TestComments.java
+++ src/main/java/org/alpha/shruti/TestComments.java
@@ -1,6 +1,7 @@
 package org.alpha.shruti;
 
 public class TestComments {
+
     public static void main(String[] args) {
         System.out.println("First line");
         System.out.println("Second line");
