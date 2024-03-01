5137f8bdadcae21c6310eca201c98fa123fa63d6
tejashriboppa <tejashriboppa@gmail.com>
2024-03-01 20:38:52 +0530

    ALPHA-TEJASHRI-1: Testing Overloading in Constructors

---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/polymorphism/methodoverloading/inconstructors/TestOverloadingConstructors.java
@@ -0,0 +1,28 @@
+package org.alpha.tejashri.oopsconcepts.polymorphism.methodoverloading.inconstructors;
+
+public class TestOverloadingConstructors {
+
+    // Constructor name should same as class name
+    public TestOverloadingConstructors(int age) {
+        System.out.println("I am in int Parameter Constructor : ");
+        System.out.println(age);
+    }
+
+    public TestOverloadingConstructors(String str) {
+        System.out.println("I am in String Parameter Constructor : ");
+        System.out.println(str);
+    }
+
+    // These above 2 Constructors are overloaded -in same class, 2 same Constructors names,
+    // with diff parameters, with diff logic/behaviour
+    public static void main(String[] args) {
+        new TestOverloadingConstructors(100);
+        new TestOverloadingConstructors("Tejashri");
+    }
+}
+
+//----------- Output --------
+//I am in int Parameter Constructor :
+//100
+//I am in String Parameter Constructor :
+//Tejashri
