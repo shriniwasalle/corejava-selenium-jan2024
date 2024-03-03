e401fb18ef4f06cdf560c46874d66e33934380b1
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-03-03 23:57:09 +0530

    ALPHA-SANJEEVANI-01: created a new program of overloading concept.

---
+++ src/main/java/org/alpha/sanjeevani/oopsconcepts/polymorphism/overloading/TestOverLoading2.java
@@ -0,0 +1,20 @@
+package org.alpha.sanjeevani.oopsconcepts.polymorphism.overloading;
+
+public class TestOverLoading2 {
+    public void getData(String str,int age){
+        System.out.println("I am in String ,int method ");
+        System.out.println(str);
+        System.out.println(age);
+    }
+    public void getData(int age,String str){
+        System.out.println("I am in int,String method ");
+        System.out.println(age);
+        System.out.println(str);
+
+    }
+    public static void main(String[] args) {
+        new TestOverLoading2().getData("vedant",6);
+        new TestOverLoading2().getData(4,"shlok");
+
+    }
+}
