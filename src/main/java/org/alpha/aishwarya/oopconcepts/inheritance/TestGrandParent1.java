4e34e69ffb5a9e55544493096a2b118870a8033c
aishwarya <aishwaryarathod914@gmail.com>
2024-03-03 19:52:31 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Inheritance- Multilevel

---
+++ src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestGrandParent1.java
@@ -0,0 +1,6 @@
+package org.alpha.aishwarya.oopconcepts.inheritance;
+
+// by default object class extends here and can access Object class method on Child class
+public class TestGrandParent1 {
+    int age = 07;
+}
---
+++ src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestParent1.java
@@ -0,0 +1,9 @@
+package org.alpha.aishwarya.oopconcepts.inheritance;
+
+public class TestParent1 extends TestGrandParent1 {
+    int id = 10;
+
+    public void getDetails() {
+        System.out.println("I am learning Mutilevel Inheritance");
+    }
+}
