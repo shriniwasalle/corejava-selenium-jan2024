6b833bfcfe90101363657fc0165df84e9ea6338b
tejashriboppa <tejashriboppa@gmail.com>
2024-03-03 21:15:43 +0530

    ALPHA-TEJASHRI-1: Testing Method Overriding without parameters

---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/polymorphism/methodoverriding/withoutparameters/TestParentMethodOverriding.java
@@ -0,0 +1,11 @@
+package org.alpha.tejashri.oopsconcepts.polymorphism.methodoverriding.withoutparameters;
+
+public class TestParentMethodOverriding {
+
+    public void  marry(){
+        //Logic of parent choice girl
+
+        System.out.println("Inside Test parent :: marry method");
+    }
+
+}
