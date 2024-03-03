039aafda8d01889a67e36cae39a765441911b8df
aishwarya <aishwaryarathod914@gmail.com>
2024-03-03 19:16:46 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Inheritance

--- src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestChild.java
+++ src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestChild.java
@@ -2,12 +2,24 @@ package org.alpha.aishwarya.oopconcepts.inheritance;
 
 public class TestChild extends TestParent{
 
+    int age = 100;
+
     public void getData(){
         System.out.println(age);
+        System.out.println(super.age);
+        System.out.println(this.age);
+        System.out.println(name);
+    }
+
+    public void m1(){
+        System.out.println("I am in Test Child method");
+        super.m1();
     }
 
  public static void main(String[] args){
      TestChild obj = new TestChild();
       obj.getData();
+      obj.getDetails();
+      obj.m1();
     }
 }
--- src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestParent.java
+++ src/main/java/org/alpha/aishwarya/oopconcepts/inheritance/TestParent.java
@@ -3,5 +3,15 @@ package org.alpha.aishwarya.oopconcepts.inheritance;
 public class TestParent {
 
     int age =10;
+    static  String name = "Aish";
+
+    // Method calling
+    public void getDetails(){
+        System.out.println("I am in TestParent Class");
+    }
+    public void m1(){
+        System.out.println("I am in TestParent method");
+
+    }
 
 }
