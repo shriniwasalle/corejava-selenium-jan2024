67d431a7c4b420f8898e6f0b0355a8fb3a7b9929
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-03-04 17:21:12 +0530

    ALPHA-SANJEEVANI-01: created a new program of overriding constructor concept.

---
+++ src/main/java/org/alpha/sanjeevani/oopsconcepts/polymorphism/overriding/TestParent1.java
@@ -0,0 +1,10 @@
+package org.alpha.sanjeevani.oopsconcepts.polymorphism.overriding;
+
+public class TestParent1 {
+    public void mary(){
+        //logic of child class
+
+        System.out.println("inside the TestParent1 :: marry method");
+
+    }
+}
