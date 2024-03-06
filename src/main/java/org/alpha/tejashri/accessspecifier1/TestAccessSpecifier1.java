e424444c9cac044866db68caf76e690f4db445de
tejashriboppa <tejashriboppa@gmail.com>
2024-03-06 20:45:38 +0530

    ALPHA-TEJASHRI-1: Concept of Access Specifier

---
+++ src/main/java/org/alpha/tejashri/accessspecifier1/TestAccessSpecifier1.java
@@ -0,0 +1,23 @@
+package org.alpha.tejashri.accessspecifier1;
+
+public class TestAccessSpecifier1 {
+    public void m1() {
+        System.out.println("TestAccessSpecifier1 class :: m1 method which is public");
+    }
+
+    private void m2() {
+        System.out.println("TestAccessSpecifier1 class :: m2 method which is private");
+    }
+
+    void m3() {
+        System.out.println("TestAccessSpecifier1 class :: m3 method which is default");
+    }
+
+    protected void m4() {
+        System.out.println("TestAccessSpecifier1 class :: m4 method which is protected");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
