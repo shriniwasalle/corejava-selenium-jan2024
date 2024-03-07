0893395726e89ad1843806f7fb4860a53269782b
aishwarya <aishwaryarathod914@gmail.com>
2024-03-08 01:43:35 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Polymorphism:Overriding:Combination of Object Calling

---
+++ src/main/java/org/alpha/aishwarya/oopconcepts/polymorphism/Overridding/TestParent1.java
@@ -0,0 +1,9 @@
+package org.alpha.aishwarya.oopconcepts.polymorphism.Overridding;
+
+public class TestParent1 {
+    public void marry() {    //Family choice Boy
+        System.out.println("Inside TestParent : Marry method");
+    }
+
+
+}
