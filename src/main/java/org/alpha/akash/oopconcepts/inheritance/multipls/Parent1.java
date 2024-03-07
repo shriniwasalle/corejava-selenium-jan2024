2f5a94277342fd094865b090af7ba09834719a23
Akash Vaitala <119657113+akashvaitala05@users.noreply.github.com>
2024-03-07 09:09:13 +0530

    Core Java Concepts

---
+++ src/main/java/org/alpha/akash/oopconcepts/inheritance/multipls/Parent1.java
@@ -0,0 +1,10 @@
+package org.alpha.akash.oopconcepts.inheritance.multipls;
+
+public class Parent1 extends GrandParent{
+
+    public void m1() {
+
+        System.out.println("I am inside Parent1 :: m1 method");
+
+    }
+}
---
+++ src/main/java/org/alpha/akash/oopconcepts/inheritance/multipls/Parent2.java
@@ -0,0 +1,10 @@
+package org.alpha.akash.oopconcepts.inheritance.multipls;
+
+public class Parent2 extends GrandParent{
+
+    public void m1() {
+
+        System.out.println("I am inside Parent2 :: m1 method");
+    }
+
+}
