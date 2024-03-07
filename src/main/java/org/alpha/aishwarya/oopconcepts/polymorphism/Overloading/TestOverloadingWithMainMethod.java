e37640e0597bdf9435f2431dad17964a012e7fee
aishwarya <aishwaryarathod914@gmail.com>
2024-03-08 01:46:20 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Polymorphism:Overloading: using main method

--- src/main/java/org/alpha/aishwarya/oopconcepts/polymorphism/Overloading/TestOverloadingWithMainMethod.java
+++ src/main/java/org/alpha/aishwarya/oopconcepts/polymorphism/Overloading/TestOverloadingWithMainMethod.java
@@ -3,18 +3,18 @@ package org.alpha.aishwarya.oopconcepts.polymorphism.Overloading;
 public class TestOverloadingWithMainMethod {
 
     // main method using int parameter
-    public static void main (int[] ar){
+    public static void main(int[] ar) {
         System.out.println("I am in int array main method " + ar);
     }
 
     public static void main(String[] args) {
-        main(new int[] {10,10,20});  // data type int Array calling
+        main(new int[]{10, 10, 20});  // data type int Array calling
     }
 }
 
 
-
-/** Output ----
+/**
+ * Output ----
  * I am in int array main method [I@6aaa5eb0
  */
 
