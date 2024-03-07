4329cdfda39f6230ff1e121268bf20f9f2580f9f
tejashriboppa <tejashriboppa@gmail.com>
2024-03-07 21:03:32 +0530

    ALPHA-TEJASHRI-1: Concept of abstract class

---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/abstraction/abstractclass/TestChildAbstractClass.java
@@ -0,0 +1,32 @@
+package org.alpha.tejashri.oopsconcepts.abstraction.abstractclass;
+
+public class TestChildAbstractClass extends TestParentAbstractClass {
+
+    public void m1() {
+        System.out.println("In side TestAbstractChildClass : abstract : m1 method");
+    }
+
+    public static void main(String[] args) {
+        //abstract class as a reference can use for other class object
+        TestParentAbstractClass obj = new TestChildAbstractClass();
+        obj.m1(); //parent abstract method
+        obj.m3(); //parent Non-abstract method
+
+        TestParentAbstractClass.m4(); //parent Non-abstract static method
+        // without object can call static methods bcoz linked with classes not with objects
+
+        //abstract class object can not create
+        //TestParentAbstractClass obj = new TestParentAbstractClass();
+        //obj.m1(); //m1 is abstract method means no-body/implementation of m1 in parent class
+        //then no use to call bcoz nothing will get in output
+
+        //obj.m2(); //m2 is Non-abstract method
+        //non-abstract method can call but abstract method can not call, it's not possible
+        //so strict rule created by java that abstract class object can not be created
+    }
+}
+
+//-----------Output------------
+//In side TestAbstractChildClass : abstract : m1 method
+//In side TestAbstractParentClass : Non-abstract : m3 method
+//In side TestAbstractParentClass : static Non-abstract : m4 method
---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/abstraction/abstractclass/TestParentAbstractClass.java
@@ -0,0 +1,29 @@
+package org.alpha.tejashri.oopsconcepts.abstraction.abstractclass;
+
+public abstract class TestParentAbstractClass {
+    public String name = "Tejashri";
+
+
+    //Abstract Method
+    public abstract void m1();
+
+    // we can not declare static to be abstract methods
+    //public static abstract void m2();
+    // if declared then static methods can not be overridden
+    // & if we call this method by using class name then we don't get any implementation out of it
+
+    //Non-Abstract Method
+    public void m3(){
+        System.out.println("In side TestAbstractParentClass : Non-abstract : m3 method");
+    }
+
+    // we can declare static to be Non-abstract methods
+    public static void m4(){
+        System.out.println("In side TestAbstractParentClass : static Non-abstract : m4 method");
+    }
+
+    // In abstract class main method can create
+    public static void main(String[] args) {
+
+    }
+}
