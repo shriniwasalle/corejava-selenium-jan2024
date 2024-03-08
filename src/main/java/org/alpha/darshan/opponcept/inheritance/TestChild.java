575b268c16034718fb72469321f873bc7d652626
DarshanGaikwad26 <darshangaikwad7058@gmail.com>
2024-03-08 20:48:37 +0530

    ALPHA-DARSHAN-26 Core Java Concept

--- src/main/java/org/alpha/darshan/opponcept/inheritance/TestChild.java
+++ src/main/java/org/alpha/darshan/opponcept/inheritance/TestChild.java
@@ -8,9 +8,19 @@ public class TestChild extends TestParent {
         System.out.println(age);
     }
 
+    public  void m2(){
+        System.out.println("i am in m2 method");
+    }
+
     public static void main(String[] args) {
         TestChild t1= new TestChild();
         t1.methodChild();
+        TestParent p1= new TestChild();
+        //p1.m2();
+        /*TestParent : reference
+        TestChild: object
+        bcz we are giving reference of test parent then compiler will check
+        is there any method present in TestParent class if not then it will give error*/
 
     }
 }
