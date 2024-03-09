af3ca8bea5326d9dc08c9bbfebb3d91357b6d90e
aishwarya <aishwaryarathod914@gmail.com>
2024-03-10 00:41:30 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Polymorphism:Overriding:Using Mutiple parameters

---
+++ src/main/java/org/alpha/aishwarya/oopconcepts/polymorphism/Overridding/TestParent3.java
@@ -0,0 +1,8 @@
+package org.alpha.aishwarya.oopconcepts.polymorphism.Overridding;
+
+public class TestParent3 {
+    public void m1(int age, String str) {
+        System.out.println("Inside TestParent method " + age + str);
+    }
+
+}
