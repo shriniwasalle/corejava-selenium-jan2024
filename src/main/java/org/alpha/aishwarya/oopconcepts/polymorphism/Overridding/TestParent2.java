2137a4e518681a6a280cfd737b751c25a2d2e1c3
aishwarya <aishwaryarathod914@gmail.com>
2024-03-09 23:24:54 +0530

    ALPHA-AISHWARYA-07: Core Java Concepts:Polymorphism:Overriding:Run Time Polymorphism

---
+++ src/main/java/org/alpha/aishwarya/oopconcepts/polymorphism/Overridding/TestParent2.java
@@ -0,0 +1,13 @@
+package org.alpha.aishwarya.oopconcepts.polymorphism.Overridding;
+
+public class TestParent2 {
+    public void m1() {
+        System.out.println("Inside Parent m1 method");
+    }
+
+    public void m2() {
+        System.out.println("Inside TestParent M2 Method ");
+    }
+
+
+}
