53529a956812c136b11e304503b35a0990b8e0c6
tejashriboppa <tejashriboppa@gmail.com>
2024-03-09 21:03:15 +0530

    ALPHA-TEJASHRI-1: Testing constructors in abstract class

---
+++ src/main/java/org/alpha/tejashri/oopsconcepts/abstraction/abstractclass/constractors/TestConstructorInACParent.java
@@ -0,0 +1,9 @@
+package org.alpha.tejashri.oopsconcepts.abstraction.abstractclass.constractors;
+
+public abstract class TestConstructorInACParent {
+    public TestConstructorInACParent() {
+        System.out.println("Inside abstract class Constructor");
+    }
+
+    public abstract void m1();
+}
