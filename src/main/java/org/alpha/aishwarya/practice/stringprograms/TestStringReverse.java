1636b92fefef55bcad70d051ff1033fb05e8cfe3
aishwarya <aishwaryarathod914@gmail.com>
2024-03-10 02:00:46 +0530

    ALPHA-AISHWARYA-07:Core Java:String Practice:Reverse String

---
+++ src/main/java/org/alpha/aishwarya/practice/stringprograms/TestStringReverse.java
@@ -0,0 +1,18 @@
+package org.alpha.aishwarya.practice.stringprograms;
+
+public class TestStringReverse {
+    public static void main(String[] args) {
+
+
+        String str = "Aishwarya";
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+
+    }
+}
+/**
+ * --Output--
+ * ayrawhsiA
+ */
+
