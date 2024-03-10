1e01fe702b97e46a24b6a950337c1d095df8bc7a
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-03-10 15:55:08 +0530

    ALPHA-SANJEEVANI-01: created a new program for practice .

---
+++ src/main/java/org/alpha/sanjeevani/oopsconcepts/abstraction/abstractclass/TestConstructorParentAc.java
@@ -0,0 +1,11 @@
+package org.alpha.sanjeevani.oopsconcepts.abstraction.abstractclass;
+
+public abstract class TestConstructorParentAc {
+
+    public TestConstructorParentAc(){
+        System.out.println("I am in TestConstructorParentAc constructor ");
+    }
+    public abstract void m1();
+
+    }
+
