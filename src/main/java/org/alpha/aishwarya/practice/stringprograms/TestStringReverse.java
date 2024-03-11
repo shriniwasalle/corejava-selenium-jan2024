e1be508af3b254d175d1f5f7d83a524e90f40b41
aishwarya <aishwaryarathod914@gmail.com>
2024-03-12 01:06:24 +0530

    ALPHA-AISHWARYA-07:Core Java: Practice:String Reverse program

--- src/main/java/org/alpha/aishwarya/practice/stringprograms/TestStringReverse.java
+++ src/main/java/org/alpha/aishwarya/practice/stringprograms/TestStringReverse.java
@@ -3,11 +3,15 @@ package org.alpha.aishwarya.practice.stringprograms;
 public class TestStringReverse {
     public static void main(String[] args) {
 
-
         String str = "Aishwarya";
 
         StringBuilder sb = new StringBuilder(str);
         System.out.println(sb.reverse());
+
+
+        //Using StringBuffer
+         StringBuffer sb1 = new StringBuffer(str);
+        System.out.println(sb1.reverse());
 
     }
 }
