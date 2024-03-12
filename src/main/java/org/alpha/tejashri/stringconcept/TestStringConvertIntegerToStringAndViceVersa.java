300e42f1450d3b9ee21d45ce0816f006d20deec6
tejashriboppa <tejashriboppa@gmail.com>
2024-03-12 19:27:38 +0530

    ALPHA-TEJASHRI-1: Testing an String Convert Integer To String And Vice Versa

---
+++ src/main/java/org/alpha/tejashri/stringconcept/TestStringConvertIntegerToStringAndViceVersa.java
@@ -0,0 +1,22 @@
+package org.alpha.tejashri.stringconcept;
+
+public class TestStringConvertIntegerToStringAndViceVersa {
+
+    public static void main(String[] args) {
+
+        // Convert int to String
+        int i = 100;
+        String str1 = Integer.toString(i);
+        System.out.println(str1);
+
+        // Convert int to String using valueOf
+        int j = 1000;
+        String str2 = String.valueOf(j);
+        System.out.println(str2);
+
+        // Convert String to int
+        String str3 = "10000";
+        int k = Integer.parseInt(str3);
+        System.out.println(k);
+    }
+}
