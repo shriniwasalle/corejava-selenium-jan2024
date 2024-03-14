34d9b32fbb6762f07c02ac4f94dff1ecb666088d
aishwarya <aishwaryarathod914@gmail.com>
2024-03-15 01:19:13 +0530

    ALPHA-AISHWARYA-07:Core Java:Abstraction Concept

---
+++ src/main/java/org/alpha/aishwarya/abstraction/TestAbstractClass.java
@@ -0,0 +1,18 @@
+package org.alpha.aishwarya.abstraction;
+
+public abstract class TestAbstractClass {
+    public String name ="Aishwarya";
+
+    public  abstract void wheels();
+
+    public abstract void m1();
+
+    public void m2(){
+
+        System.out.println("Inside TestAbstractClass Parent : m2 Method");
+    }
+
+
+
+
+}
---
+++ src/main/java/org/alpha/aishwarya/abstraction/TestBike.java
@@ -0,0 +1,31 @@
+package org.alpha.aishwarya.abstraction;
+
+import org.alpha.shriniwas.oopsconcepts.abstraction.abstractclass.TestAbstractClass;
+
+public class TestBike extends TestAbstractClass {
+
+        @Override
+        public void wheels() {
+            System.out.println("2 wheels");
+
+        }
+
+        @Override
+        public void m1() {
+            System.out.println("Inside TestAbstractClass Bike : m1 Method");
+
+        }
+
+    public static void main(String[] args) {
+            TestAbstractClass obj = new TestBike();
+       obj.m2();
+        obj.m1();
+        obj.wheels();
+    }
+}
+/** -- Output---
+ *
+ Inside TestAbstractClass :: m2 method
+ Inside TestAbstractClass Bike : m1 Method
+ 2 wheels
+ */
