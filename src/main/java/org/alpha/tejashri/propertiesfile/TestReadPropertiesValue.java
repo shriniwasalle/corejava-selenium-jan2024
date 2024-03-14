7e8b904313bca267720a084a13f0b4e3a218c77c
tejashriboppa <tejashriboppa@gmail.com>
2024-03-14 19:55:14 +0530

    ALPHA-TEJASHRI-1: Testing Properties file

---
+++ src/main/java/org/alpha/tejashri/propertiesfile/TestReadPropertiesValue.java
@@ -0,0 +1,24 @@
+package org.alpha.tejashri.propertiesfile;
+
+import java.io.IOException;
+
+public class TestReadPropertiesValue {
+
+    public static void main(String[] args) throws IOException {
+        TestProperties obj = new TestProperties();
+        String userName = obj.getPropertyValues("user.name"); //value returned from getPropertyValues method & stored in variable to perform further operation
+        System.out.println("Finding length of user name : " + userName.length()); //performed length operation on userName variable which is string
+
+        String password = obj.getPropertyValues("user.password");
+        System.out.println("Finding length of user password : " + password.length());
+
+        String str = "Admin";
+
+        if (str.equals("Admin")) {
+            obj.getPropertyValues("role.admin");
+        } else if (str.equals("operator")) {
+            obj.getPropertyValues("role.operator");
+        }
+        System.out.println(obj);
+    }
+}
