c1f7158a5299e8745f614f7de1092e6e78fefca1
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-03-21 18:53:57 +0530

    ALPHA-SANJEEVANI-01: created a new program of collection set .

---
+++ src/main/java/org/alpha/sanjeevani/collections/set/TestLinkedHashSet.java
@@ -0,0 +1,18 @@
+package org.alpha.sanjeevani.collections.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TestLinkedHashSet {
+    public static void main(String[] args) {
+        Set<String> set=new LinkedHashSet<>();
+        set.add("java4");
+        set.add("java1");
+        set.add("java2");
+        set.add("java3");
+
+        System.out.println("set is:"+set);
+
+    }
+}
---
+++ src/main/java/org/alpha/sanjeevani/collections/set/TestTreeSet.java
@@ -0,0 +1,22 @@
+package org.alpha.sanjeevani.collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TestTreeSet {
+    public static void main(String[] args) {
+
+        Set<String> set = new TreeSet<>();// in tree set element print with alphabetic order that means sorting order
+        set.add("java4");
+        set.add("java2");
+        set.add("java1");
+        set.add("java3");
+        // set.add(null); when we add null but null has no value, that’s why the compareTo() method cannot compare null with another value, giving a NullPointerException.
+        //
+        //add method declaration
+
+
+        System.out.println("Treeset is:" + set);
+    }
+}
