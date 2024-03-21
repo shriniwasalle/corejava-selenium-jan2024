905c8caa0aadba9c918afbaf04bcf5b5d0280b5a
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-03-21 16:32:46 +0530

    ALPHA-SANJEEVANI-01: created a new program of properties file .

---
+++ src/main/java/org/alpha/sanjeevani/propertiesfile/TestProperties.java
@@ -0,0 +1,37 @@
+package org.alpha.sanjeevani.propertiesfile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+    public static void main(String[] args) throws IOException {
+
+        /*System.out.println("https://www.google.co.in/");
+        String userName="sanjeevani";
+        String password="sanju7028";
+        System.out.println("enter your userName :"+userName);
+        System.out.println("enter your password :"+password);
+*/
+
+        File file = new File("C:\\Users\\Dell\\corejava-selenium-jan2024\\src\\main\\java\\org\\alpha\\sanjeevani\\propertiesfile\\config.properties");
+
+        FileInputStream fileinputstream = new FileInputStream(file);
+
+        Properties prop = new Properties();
+        prop.load(fileinputstream);
+
+        String url = prop.getProperty("url"); // url value
+        System.out.println(url);
+
+        String userneme = prop.getProperty("user.name"); // url value
+        System.out.println(userneme);
+
+        String password = prop.getProperty("user.password"); // url value
+        System.out.println(password);
+
+
+    }
+}
