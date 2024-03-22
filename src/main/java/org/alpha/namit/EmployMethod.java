f18cd6ceffc3f31562e65041022cc117e23876ce
patilnamit <patilnamit84@gmail.com>
2024-03-22 13:00:23 +0530

    Created Employ Metod

---
+++ src/main/java/org/alpha/namit/EmployMethod.java
@@ -0,0 +1,11 @@
+package org.alpha.namit;
+
+public class EmployMethod {
+    String empName = "namit";
+    int empId = 777;
+
+    public void getEmpData() {
+        System.out.println(empName);
+        System.out.println(empId);
+    }
+}
