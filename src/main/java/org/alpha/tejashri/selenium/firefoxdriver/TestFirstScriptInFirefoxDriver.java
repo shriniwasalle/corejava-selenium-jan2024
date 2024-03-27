67a57223ef4d4efe3a67eec373d7dbd21b3b7cfc
tejashriboppa <tejashriboppa@gmail.com>
2024-03-27 18:40:20 +0530

    ALPHA-TEJASHRI-1: Testing the First Script in Firefox Driver

---
+++ src/main/java/org/alpha/tejashri/selenium/firefoxdriver/TestFirstScriptInFirefoxDriver.java
@@ -0,0 +1,27 @@
+package org.alpha.tejashri.selenium.firefoxdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestFirstScriptInFirefoxDriver {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "D:\\Tejashri\\Drivers\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.manage().window().maximize();
+
+        String title = driver.getTitle();
+        System.out.println("Title : " + title);//Swag Labs
+
+        System.out.println("Current Url : " + driver.getCurrentUrl());
+
+        String pageSource = driver.getPageSource();
+        System.out.println("Page Source : " + pageSource);
+
+        driver.close();
+        driver.quit();
+    }
+}
