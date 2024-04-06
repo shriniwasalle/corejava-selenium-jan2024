d960e881a6a4231ee5824d78cfe2507ef35dcf50
ganeshkantli <ganeshkantli47@gmail.com>
2024-04-06 19:52:55 +0530

    Update

--- src/main/java/org/alpha/ganesh/HelloWorld.java
+++ src/main/java/org/alpha/ganesh/HelloWorld.java
@@ -3,5 +3,6 @@ package org.alpha.ganesh;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hellow World");
+        System.out.println("g");
     }
 }
