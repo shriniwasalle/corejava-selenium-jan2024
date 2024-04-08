d946bc11c55d6c55f47b1724b5417bfce8a42c57
shriniwasalle <shriniwasalle4@gmail.com>
2024-04-08 22:30:51 +0530

    Page Object Model 1

---
+++ src/main/java/org/alpha/shriniwas/seleniumconcepts/pom1/LoginPage.java
@@ -0,0 +1,14 @@
+package org.alpha.shriniwas.seleniumconcepts.pom1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    public void loginToApplication(WebDriver driver) {
+        // Page Related
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
