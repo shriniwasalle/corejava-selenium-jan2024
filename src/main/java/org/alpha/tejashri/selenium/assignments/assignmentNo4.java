86896d5407524fd299e93735c91603f3deaac515
tejashriboppa <tejashriboppa@gmail.com>
2024-04-08 23:02:21 +0530

    ALPHA-TEJASHRI-1: Assignment No 4

--- src/main/java/org/alpha/tejashri/selenium/assignments/assignmentNo4.java
+++ src/main/java/org/alpha/tejashri/selenium/assignments/assignmentNo4.java
@@ -8,3 +8,27 @@ import org.testng.annotations.Test;
 public class assignmentNo4 {
     public assignmentNo4() {
     }
+
+    @Test
+    public void verifyTitleChar() {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.flipkart.com/");
+
+        driver.manage().window().maximize();
+
+        System.out.println("Title : " + driver.getTitle());
+
+        String title = driver.getTitle();
+
+        System.out.println("Total Characters in Title : " + title.toCharArray().length);
+        if (title.toCharArray().length >= 10) {
+            System.out.println("Title has more than 10 char");
+        } else {
+            System.out.println("Title has less than 10 char");
+        }
+
+    }
+}
