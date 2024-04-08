f969958ecfe8f257e0cfb1f895b55d551202ec31
tejashriboppa <tejashriboppa@gmail.com>
2024-04-08 23:02:02 +0530

    ALPHA-TEJASHRI-1: Assignment No 4

---
+++ src/main/java/org/alpha/tejashri/selenium/assignments/assignmentNo4.java
@@ -0,0 +1,10 @@
+package org.alpha.tejashri.selenium.assignments;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class assignmentNo4 {
+    public assignmentNo4() {
+    }
