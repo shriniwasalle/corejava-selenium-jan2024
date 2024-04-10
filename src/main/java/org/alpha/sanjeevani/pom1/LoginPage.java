1d5ed43d20d06f02155d4463e8a8364c960c635d
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-04-10 13:42:44 +0530

    ALPHA-SANJEEVANI-01: Creating new files of Page Object Model(POM).

---
+++ src/main/java/org/alpha/sanjeevani/pom1/LoginPage.java
@@ -0,0 +1,13 @@
+package org.alpha.sanjeevani.pom1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    public void loginToApplication(WebDriver driver) {
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
---
+++ src/main/java/org/alpha/sanjeevani/pom1/ProductsPage.java
@@ -0,0 +1,14 @@
+package org.alpha.sanjeevani.pom1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ProductsPage {
+
+    public String getProductPageHeading(WebDriver driver) {
+
+        WebElement Heading = driver.findElement(By.xpath("//span[@data-test='title']"));
+        return Heading.getText();
+    }
+}
