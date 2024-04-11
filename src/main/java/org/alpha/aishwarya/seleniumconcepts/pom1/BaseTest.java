3059eb202069f7c1bf2d8945a35a92ba1a879132
aishwarya <aishwaryarathod914@gmail.com>
2024-04-11 23:23:06 +0530

    ALPHA-AISHWARYA-07: Selenium :Page Object Model(POM1)

---
+++ src/main/java/org/alpha/aishwarya/seleniumconcepts/pom1/BaseTest.java
@@ -0,0 +1,34 @@
+package org.alpha.aishwarya.seleniumconcepts.pom1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+    WebDriver driver;
+
+    public WebDriver initBrowser(String url, String browser) {   // Multiple param use: baherun access kraycha aahe
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        }
+        driver.get(url);
+        return driver;
+    }
+}
+
+
+
+
---
+++ src/main/java/org/alpha/aishwarya/seleniumconcepts/pom1/ProductsPage.java
@@ -0,0 +1,14 @@
+package org.alpha.aishwarya.seleniumconcepts.pom1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ProductsPage {
+
+    public String getProductsPageHeading(WebDriver driver) {  // get method return type = String
+        WebElement heading = driver.findElement(By.xpath("//span[@data-test= 'title']"));
+
+        return heading.getText(); // return kraycha karan testLoginpage class access
+    }
+}
