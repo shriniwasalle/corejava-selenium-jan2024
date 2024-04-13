54797c545dd3df9e0e26e8a6b67a1465798d812e
tejashriboppa <tejashriboppa@gmail.com>
2024-04-13 18:04:39 +0530

    ALPHA-TEJASHRI-1: Assignment No 6 Completed

---
+++ src/main/java/org/alpha/tejashri/selenium/assignments/AssignmentNo6/pages/CheckOutPage.java
@@ -0,0 +1,26 @@
+package org.alpha.tejashri.selenium.assignments.AssignmentNo6.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckOutPage {
+
+    private final WebDriver driver;
+    private final By firstName = By.id("first-name");
+    private final By lastName = By.id("last-name");
+    private final By postalCode = By.id("postal-code");
+    private final By bntContinue = By.id("continue");
+
+    //created parametrised constructor
+    public CheckOutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void UserInfo(String fName, String lName, String pinCode) {
+        driver.findElement(firstName).sendKeys(fName);
+        driver.findElement(lastName).sendKeys(lName);
+        driver.findElement(postalCode).sendKeys(pinCode);
+        driver.findElement(bntContinue).click();
+    }
+
+}
