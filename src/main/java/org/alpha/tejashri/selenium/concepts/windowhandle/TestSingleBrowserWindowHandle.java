d0b885b104403a322bf7f532f360ea6ca274dbb9
tejashriboppa <tejashriboppa@gmail.com>
2024-04-16 18:15:47 +0530

    ALPHA-TEJASHRI-1: Button 1 - Testing Single Browser Window Handle

--- src/main/java/org/alpha/tejashri/selenium/concepts/windowhandle/TestSingleBrowserWindowHandle.java
+++ src/main/java/org/alpha/tejashri/selenium/concepts/windowhandle/TestSingleBrowserWindowHandle.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -18,39 +19,47 @@ public class TestSingleBrowserWindowHandle extends BaseCodeTest {
         driver = initBrowser("https://www.hyrtutorials.com/p/window-handles-practice.html#google_vignette", "chrome");
     }
 
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
     @Test
     public void verifySingleWindowHandle() {
-        String parentWindow = driver.getWindowHandle(); // To get the unique id of the window
+        String parentBrowserWindow = driver.getWindowHandle(); //To get the unique id of the Browser window
 
-        System.out.println("Parent Window Id : " + parentWindow);// parent id -123
+        System.out.println("Parent Browser Window Id : " + parentBrowserWindow);// parent id -123
+        System.out.println("---------------------------------------------------");
 
         driver.findElement(By.id("newWindowBtn")).click(); //This button is used for opening a new browser window
 
-        //After clicking on that button, child window will also open
-        //Now 2 windows are open, so use set<String>
-        Set<String> windows = driver.getWindowHandles();
-        System.out.println("Multiple Window Id's : " + windows);// [parent id-123, child id-456]-order not preserved
+        //After clicking on that button, child Browser window will also open
+        //Now 2 Browser windows are open, so use set<String>
+        Set<String> BrowserWindows = driver.getWindowHandles();
+        System.out.println("Multiple Window Id's : " + BrowserWindows);// [parent id-123, child id-456]-order not preserved
+        System.out.println("---------------------------------------------------");
 
-        //to iterate the window ids with for each
-        for (String window : windows) {
-            System.out.println(window);
+        System.out.println("Iterating the Browser Window Id's with for each loop");
+        //to iterate the Browser window ids with for each loop
+        for (String BrowserWindow : BrowserWindows) {
+            System.out.println(BrowserWindow);
 
             //Check the condition that
-            // window != parentWindow --> Switch the focus to childWindow
-            // window == parentWindow --> Don't Switch/skip
+            // BrowserWindow != parentBrowserWindow --> Switch the focus to childBrowserWindow
+            // BrowserWindow == parentBrowserWindow --> Don't Switch/skip
 
-            //!parentWindow.equals(window) --> Switch
-            //parentWindow.equals(window) -->  skip
+            //!parentBrowserWindow.equals(BrowserWindow) --> Switch
+            //parentBrowserWindow.equals(BrowserWindow) -->  skip
 
             //example ids
-            // !465.equals(123) --> !true = false --> Switch the focus to childWindow
+            // !465.equals(123) --> !true = false --> Switch the focus to childBrowserWindow
             //123.equals123 --> true --> Don't Switch/skip
 
-            //Checking the condition
-            if (!parentWindow.equals(window)) {
-                driver.switchTo().window(window);// To Switch the focus to child Window
-                driver.manage().window().maximize();//To maximize the child window
-                System.out.println("Child Window Title : " + driver.getTitle());
+            System.out.println("---------------------------------------------------");
+            //Checking the condition of parent & child Ids
+            if (!parentBrowserWindow.equals(BrowserWindow)) {
+                driver.switchTo().window(BrowserWindow);// To Switch the focus to child Browser Window
+                System.out.println("Child Browser Window Title : " + driver.getTitle());
 
                 driver.findElement(By.id("firstName")).sendKeys("Abc");
                 driver.findElement(By.id("lastName")).sendKeys("Xyz");
@@ -69,14 +78,15 @@ public class TestSingleBrowserWindowHandle extends BaseCodeTest {
 
                 Assert.assertEquals(actualRegistrationMssg, expectedRegistrationMssg);
 
-                //driver.close();//To close the current focused window ie child Window only
+                //driver.close();//To close the current focused window ie child Browser Window only
             }
         }
 
-        driver.switchTo().window(parentWindow);
-        System.out.println("Parent Window Title : " + driver.getTitle());
+        System.out.println("---------------------------------------------------");
+        driver.switchTo().window(parentBrowserWindow);
+        System.out.println("Parent Browser Window Title : " + driver.getTitle());
 
-        //driver.close();// To close the current focused window ie parent window only
-        driver.quit();// To close the multiple windows opened by driver ie both parent & child window
+        //driver.close();// To close the current focused window ie parent Browser window only
+        //driver.quit();// To close the multiple windows opened by driver ie both parent & child Browser windows
     }
 }
