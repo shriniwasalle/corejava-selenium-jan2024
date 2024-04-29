d69b125d10c7587eae49db315ac98575c63017c1
aishwarya <aishwaryarathod914@gmail.com>
2024-04-29 17:05:49 +0530

    ALPHA-AISHWARYA-07: Selenium : Concepts

---
+++ src/main/java/org/alpha/aishwarya/selenium/assignments/TestRegistration.java
@@ -0,0 +1,15 @@
+package org.alpha.aishwarya.selenium.assignments;
+
+
+import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+
+public class TestRegistration {
+
+    public void VerifyRegistration(){
+
+
+    }
+
+
+
+}
