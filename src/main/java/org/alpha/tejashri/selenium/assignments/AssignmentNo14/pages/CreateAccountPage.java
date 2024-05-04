65f6673cbe691c8f7a810bb2af44c3886aae559f
shriniwasalle <shriniwasalle4@gmail.com>
2024-05-04 21:27:27 +0530

    Page Actions

--- src/main/java/org/alpha/tejashri/selenium/assignments/AssignmentNo14/pages/CreateAccountPage.java
+++ src/main/java/org/alpha/tejashri/selenium/assignments/AssignmentNo14/pages/CreateAccountPage.java
@@ -1,10 +1,13 @@
 package org.alpha.tejashri.selenium.assignments.AssignmentNo14.pages;
 
+import org.alpha.tejashri.selenium.assignments.AssignmentNo14.pageactions.PageActions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CreateAccountPage {
     private final WebDriver driver;
+    PageActions actions;
     private final By linkCreate = By.linkText("Create an Account");
 
     private final By firstName = By.id("firstname");
@@ -18,21 +21,27 @@ public class CreateAccountPage {
 
     public CreateAccountPage(WebDriver driver) {
         this.driver = driver;
+        actions = new PageActions(driver);
     }
 
     public void verityCreateAccountPage() {
-        driver.findElement(linkCreate).click();
+        // driver.findElement(linkCreate).click();
+        actions.clickOnElement(linkCreate);
 
         //Personal Information page
-        driver.findElement(firstName).sendKeys("Abc");
-        driver.findElement(lastName).sendKeys("Xyz");
+        // driver.findElement(firstName).sendKeys("Abc");
+        actions.enterText(firstName, "Abc");
+        actions.enterText(lastName, "Xyz");
+        /// driver.findElement(lastName).sendKeys("Xyz");
 
         //Sign-in Information
-        driver.findElement(email).sendKeys("abc123xyz@example.com");
-        driver.findElement(password).sendKeys("abcxyz@123");
-        driver.findElement(confirmPassword).sendKeys("abcxyz@123");
-        driver.findElement(btnSubmit).click();
+        actions.enterText(email, "abc123xyz@example.com");
+        actions.enterText(password, "abcxyz@123");
+        actions.enterText(confirmPassword, "abcxyz@123");
+//        driver.findElement(email).sendKeys("abc123xyz@example.com");
+//        driver.findElement(password).sendKeys("abcxyz@123");
+//        driver.findElement(confirmPassword).sendKeys("abcxyz@123");
+        // driver.findElement(btnSubmit).click();
+        actions.clickOnElement(btnSubmit);
     }
-
-
 }
