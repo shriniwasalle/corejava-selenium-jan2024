e480edd68f01d3bc0c49e901f2b178d04367cb47
sanjeevani70 <sanjeevsutar14@gmail.com>
2024-06-18 16:19:11 +0530

    ALPHA-SANJEEVANI-01: existing file.

--- src/main/java/org/alpha/sanjeevani/seleniumconcepts/TestSeleniumLocators1.java
+++ src/main/java/org/alpha/sanjeevani/seleniumconcepts/TestSeleniumLocators1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
-public class TestSeleniumLocators1 {
+public class    TestSeleniumLocators1 {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
